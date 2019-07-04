96a90952194ea72699136851725c1d0ec4ace4ca
Julia Robinson <juliaarobinson@hotmail.com>
2019-07-04 21:44:05 +0100

    Updates

---
+++ precache-manifest.ddd29d9f920171df10edda08fcbc8517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f7f83be0a77522cc903",
+    "url": "/calculator-react/static/css/main.68d7c885.chunk.css"
+  },
+  {
+    "revision": "0f7f83be0a77522cc903",
+    "url": "/calculator-react/static/js/main.0f7f83be.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/calculator-react/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "180f923038051d0b454f",
+    "url": "/calculator-react/static/js/runtime~main.180f9230.js"
+  },
+  {
+    "revision": "34724322f11b6369a2297330c6a37f00",
+    "url": "/calculator-react/index.html"
+  }
+];
